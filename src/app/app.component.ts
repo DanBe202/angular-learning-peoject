a27a337c8d7901d88b7de0a1fefb16ecc68fd26b
d_b <opaetdaniel2004@gmail.com>
2025-10-30 17:40:22 +0200

    Refactor app component imports by removing unused modules for cleaner structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {CardComponent} from './components/card/card.component';
-import {JsonPipe} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    CardComponent,
-    JsonPipe,
     RouterLink,
   ],
   templateUrl: './app.component.html',
